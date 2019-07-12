fabcc17efc56515b35c8231020ee2408bae1e1e1
Ahsan Ahmed <ahsan.ahmed@doozielabs.com>
2019-07-12 15:40:21 +0500

    Style Added

--- graphql-frontend/src/components/AddBook.js
+++ graphql-frontend/src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { graphql, compose } from 'react-apollo';
-import { getAuthorsQuery, addBookMutation } from '../queries/queries';
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries/queries';
 
 class AddBook extends Component {
     constructor(props) {
@@ -28,7 +28,8 @@ class AddBook extends Component {
                 name: this.state.name,
                 genre: this.state.genre,
                 authorid: this.state.authorid
-            }
+            },
+            refetchQueries: [{ query: getBooksQuery }]
         });
     }
     render() {
