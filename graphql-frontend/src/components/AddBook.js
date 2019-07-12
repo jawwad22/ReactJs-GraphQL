51424abf0246a1d904205d1665fc8be762f1469c
Ahsan Ahmed <ahsan.ahmed@doozielabs.com>
2019-07-12 15:16:44 +0500

    Updating Component State

--- graphql-frontend/src/components/AddBook.js
+++ graphql-frontend/src/components/AddBook.js
@@ -3,32 +3,44 @@ import { graphql } from 'react-apollo';
 import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
-    displayAuthors(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            genre: '',
+            authorid: ''
+        }
+    }
+    displayAuthors() {
         var data = this.props.data;
-        if(data.loading){
-            return( <option disabled>Loading authors</option> );
+        if (data.loading) {
+            return (<option disabled>Loading authors</option>);
         } else {
             return data.authors.map(author => {
-                return( <option key={ author.id } value={author.id}>{ author.name }</option> );
+                return (<option key={author.id} value={author.id}>{author.name}</option>);
             });
         }
     }
-    render(){
-        return(
-            <form id="add-book">
+    submitForm(e) {
+        e.preventDefault();
+        console.log(this.state)
+    }
+    render() {
+        return (
+            <form id="add-book" onSubmit={this.submitForm.bind(this)}>
                 <div className="field">
                     <label>Book name:</label>
-                    <input type="text" />
+                    <input type="text" onChange={(e) => this.setState({ name: e.target.value })} />
                 </div>
                 <div className="field">
                     <label>Genre:</label>
-                    <input type="text" />
+                    <input type="text" onChange={(e) => this.setState({ genre: e.target.value })} />
                 </div>
                 <div className="field">
                     <label>Author:</label>
-                    <select>
+                    <select onChange={(e) => this.setState({ authorid: e.target.value })}>
                         <option>Select author</option>
-                        { this.displayAuthors() }
+                        {this.displayAuthors()}
                     </select>
                 </div>
                 <button>+</button>
